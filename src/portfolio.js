a465a431172372a6d57e751ed270a364eb8c5512
malkavien <rafael_allx@hotmail.com>
2026-06-02 13:45:19 -0300

    atualizado curriculo

--- src/portfolio.js
+++ src/portfolio.js
@@ -206,13 +206,6 @@ const bigProjects = {
       footerLink: [{name: "GitHub", url: "https://github.com/malkavien"}]
     },
     {
-      image: require("./assets/images/ld.png"),
-      projectName: "Linkdesign",
-      projectDesc:
-        "Sistema de gestão escolar, integração com APIs bancárias e controle de acesso.",
-      footerLink: [{name: "GitHub", url: "https://github.com/malkavien"}]
-    },
-    {
       image: require("./assets/images/nc.png"),
       projectName: "NovaCode",
       projectDesc:
